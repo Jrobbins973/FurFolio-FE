69f30bd8c81dc7f42d1237a7aa82e4f15ba30586
Joe Robbins <joe.robbins.cpt@gmail.com>
2023-01-29 08:38:47 -0500

    made some components but no functionality yet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React, {useState, useEffect} from 'react';
 import LoginPage from './components/LoginPage';
 import './App.css';
+import { Switch, Route} from "react-router-dom";
+
 
 function App() {
   return (
